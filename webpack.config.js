ccedb7f35bb6cb361f5eb7f62b8419030f1dd4f0
Chis Grimmett <chris@grimtech.net>
2018-03-15 02:33:54 -0700

    adding feathersjs

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src_client/index.js',
     output: {
         path: path.resolve(__dirname, './dist'),
         publicPath: '/',
